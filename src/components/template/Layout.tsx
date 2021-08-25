d33c05a79b7ae262fcf2ebd852cfbffd13f4e208
geancarloslc <geancarloslc@icloud.com>
2021-08-25 20:58:14 -0300

    Contruindo o menu

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -1,21 +1,21 @@
-import MenuLateral from './MenuLateral';
-import Cabecalho from './Cabecalho';
-import Conteudo from './Conteudo';
+import MenuLateral from "./MenuLateral";
+import Cabecalho from "./Cabecalho";
+import Conteudo from "./Conteudo";
 
 interface LayoutProps {
-    titulo: string,
-    subtitulo: string,
-    children?: any,
+  titulo: string;
+  subtitulo: string;
+  children?: any;
 }
 
-export default function Layout(props: LayoutProps){
-    return (
-      <div>
-        <MenuLateral />
+export default function Layout(props: LayoutProps) {
+  return (
+    <div className={`flex h-screen w-screen`}>
+      <MenuLateral />
+      <div className={`flex flex-col p-7 bg-gray-300 w-full`}>
         <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
-        <Conteudo>
-            {props.children}
-        </Conteudo>
+        <Conteudo>{props.children}</Conteudo>
       </div>
-    );
+    </div>
+  );
 }
