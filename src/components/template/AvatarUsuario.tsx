a75aa9a5291adb06b4c1b40c5b27ab069515a276
geancarloslc <geancarloslc@icloud.com>
2021-09-07 00:23:03 -0300

    Revert "Firebase ok"
    This reverts commit 83f613ec073b82635e0de6e5263161f1331af9c7.

--- src/components/template/AvatarUsuario.tsx
+++ src/components/template/AvatarUsuario.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import Image from "next/image"
 import avatar from "../assets/avatar.svg";
@@ -14,14 +13,13 @@ export default function AvatarUsuario(props: AvatarUsuario){
     const { usuario } = useAutenticacao();
     return (
       <Link href="/perfil">
-        <img
-          src={usuario?.imagemUrl ?? "/images/avatar.svg"}
-          alt="Avatar do Usuário"
-          className={`
-                    h-10 w-10 rounded-full cursor-pointer
-                    ${props.className}
-                `}
-        />
+          <Image className={`cursor-pointer`}
+            src={usuario?.imagemUrl ?? avatar}
+            height={33}
+            width={35}
+          >
+              
+          </Image>
       </Link>
     );
 }
